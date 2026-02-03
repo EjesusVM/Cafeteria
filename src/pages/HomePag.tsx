2118f0811783a53313447a94d5850411db35337e
EjesusVM <ejesusvargasmtz@gmail.com>
2026-02-02 23:08:15 -0600

    se corrigio la liga de los iconos

--- src/pages/HomePag.tsx
+++ src/pages/HomePag.tsx
@@ -19,17 +19,17 @@ export default function Home() {
                 <section className="iconos">
                     <ul className="listado-iconos">
                         <li>
-                            <img src="../public/img/icono_cafe.svg" alt="icono cafe" />
+                            <img src="/public/img/icono_cafe.svg" alt="icono cafe" />
                             <p>Cafe</p>
                         </li>
 
                         <li>
-                            <img src="../public/img/icono_postre.svg" alt="icono postre" />
+                            <img src="/public/img/icono_postre.svg" alt="icono postre" />
                             <p>Postre</p>
                         </li>
 
                         <li>
-                            <img src="../public/img/icono_te.svg" alt="icono Te" />
+                            <img src="/public/img/icono_te.svg" alt="icono Te" />
                             <p>Te</p>
                         </li>
                     </ul>
